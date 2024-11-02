480be285e658fc0a0840c43b70bd0fb1f85fa76b
Andres2580 <143357014+Andres2580@users.noreply.github.com>
2024-11-02 11:03:32 -0500

    feat(userContext): update bounded userContext

--- src/main/java/com/upecinosai/saifu/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
+++ src/main/java/com/upecinosai/saifu/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -1,4 +1,29 @@
 package com.upecinosai.saifu.platform.shared.domain.model.aggregates;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-public class AuditableAbstractAggregateRoot {
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableAbstractAggregateRoot <T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Getter
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updateAt;
 }
